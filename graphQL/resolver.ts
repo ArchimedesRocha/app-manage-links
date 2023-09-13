bb202f22b422a5e23f9fc4c1c6508b75ef94986c
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2023-09-13 20:54:58 -0300

    graphql configurado

---
+++ graphQL/resolver.ts
@@ -0,0 +1,7 @@
+import { Resolvers } from "./resolvers-types";
+
+export const resolvers: Resolvers = {
+  Query: {
+    links: async (parent, args, context) => await context.prisma.link.fingMany()
+  }
+}
